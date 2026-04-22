2a75a52c728dab46473fc3c9717fcc7fa2690c0c
wagnerwilliam <wagnerveleswilliam@alumnos.cei.es>
2026-04-22 15:07:17 +0200

    Agrega funcionalidad para sistema de colores.

--- sistema_colores/front/js/functions.js
+++ sistema_colores/front/js/functions.js
@@ -3,28 +3,47 @@ const ul = document.querySelector("ul");
 const form = document.querySelector("form");
 const inputText = document.querySelector("input[type=text]");
 const modlBorrar = document.querySelector(".modal-borrar");
-const modlEditar = document.querySelector(".modal-editar");
+const modalEditar = document.querySelector(".modal-editar");
+const botonesModalBorrar = document.querySelectorAll(".modal-borrar button");
+const botonesModalEditar = document.querySelectorAll(".modal-editar button");
+
+const previewColor = document.querySelector(".color");
+const inputsEditor = document.querySelectorAll(".modal-editar input");
+
+let colorBorrar = null;
+// colores a editar.
+let colorEditar = null;
 
 function color({ id, r, g, b }) {
+    let rgb = [r, g, b];
     let li = document.createElement("li");
-    let valor = [r, g, b].join(",");
-    console.log(valor);
+    let valor = rgb.join(",");
 
     li.style.backgroundColor = `rgb(${valor})`;
+    li.setAttribute("data-id", id)
     li.innerHTML = `
         <span>${valor}</span>
-        <button class="editarBtn">editar</button>
-        <button class="borrarBtn">borrar</button>
-    `
-    li.querySelector(".editarBtn").addEventListener("click", () => {
-        console.log("editar" + id);
-        
-        modlEditar.classList.toggle("modal-visible")
+        <button>editar</button>
+        <button>borrar</button>
+    `;
+
+    // editar
+    li.querySelector("button:nth-child(2)").addEventListener("click", () => {
+        //se agrega objeto item li a editar y rgb valores de cada input range global
+        colorEditar = { item: li, rgb };
+
+        console.log(colorEditar);
+        previewColor.style.backgroundColor = `rgb(${rgb.join(",")})`;
+        colorEditar.rgb.forEach((valor, i) => inputsEditor[i].value = valor)
+        modalEditar.classList.toggle("modal-visible");
     });
 
-    li.querySelector(".borrarBtn").addEventListener("click", () => {
-        console.log("borrar" + id);
+    // borrar
+    li.querySelector("button:nth-child(3)").addEventListener("click", () => {
+        // captura globalmente el elemento que se va a borrar.
+        colorBorrar = li;
         modlBorrar.classList.toggle("modal-visible")
+
     });
 
     return li
@@ -33,7 +52,53 @@ function color({ id, r, g, b }) {
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    console.log(inputText.value);
+    //TODO: validar input
+    let [ r, g, b ] = inputText.value.split(",");
+
+    let objColor = {
+        id: Math.random() * 3000,
+        r, g, b
+    }
     
-    //color({ id: 1, r: 1, g: 0, b: 180 });
+    ul.appendChild(color(objColor))
+
+    inputText.value = "";
+
+});
+
+// se recorre el array de nodos para obtener los botones.
+botonesModalBorrar.forEach((boton, i) => {
+    boton.addEventListener("click", () => {
+        if (i == 0) {
+            colorBorrar = null;
+            modlBorrar.classList.toggle("modal-visible")
+        } else {
+            colorBorrar.remove();
+            colorBorrar = null;
+            modlBorrar.classList.toggle("modal-visible")
+        }
+    });
+});
+
+botonesModalEditar.forEach((boton, i) => {
+    boton.addEventListener("click", () => {
+        if (i == 0) {
+            colorEditar = null;
+            modalEditar.classList.remove("modal-visible")
+        } else {
+            //opcion de guardar al editar.
+            colorEditar.item.style.backgroundColor = `rgb(${colorEditar.rgb.join(",")})`;
+            colorEditar.item.children[0].innerText = colorEditar.rgb.join(",");
+            colorEditar = null;
+            modalEditar.classList.remove("modal-visible");
+        }
+    });
+});
+
+inputsEditor.forEach((input, i) => {
+    input.addEventListener("input", () => {
+        //actualiza los valores del rgb ya que se esta usando l;a referencia de variables globales.
+        colorEditar.rgb[i] = input.value
+        previewColor.style.backgroundColor = `rgb(${colorEditar.rgb.join(",")})`;
+    });
 });
